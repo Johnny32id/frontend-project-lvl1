e66ecb05930fa2279f57cd4b916862416c07999e
Evgeniy Slizh <johnnyhandsome3232@gmail.com>
2019-11-06 21:13:22 +0300

    add brain-gcd

---
+++ src/bin/games/brain-gcd.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+import { brainEvenRules, greetings, brainGame, getRandomNumber, getRandomSign, commonDivisor } from '../..';
+import {cons, car, cdr, toString} from "@hexlet/pairs";
+greetings();
+brainEvenRules();
+
+export const getQuestionAnswer = () => {
+    const randomNumber1 = getRandomNumber(1, 100);
+    const randomNumber2 = getRandomNumber(1, 100);
+    const gcdQuestion = `${randomNumber1} ${randomNumber2}`;
+    const gcdAnswer = String(commonDivisor(randomNumber1, randomNumber2));
+    return cons(gcdQuestion, gcdAnswer);
+}
+brainGame(getQuestionAnswer);
