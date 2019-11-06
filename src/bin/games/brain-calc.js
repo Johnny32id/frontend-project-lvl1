4591537ac9a966ad01ef224439f409a4c09af430
Evgeniy Slizh <johnnyhandsome3232@gmail.com>
2019-11-06 19:46:35 +0300

    add brain-calc

--- src/bin/games/brain-calc.js
+++ src/bin/games/brain-calc.js
@@ -9,7 +9,7 @@ export const questionAnswer = () => {
     const randomNumber2 = getRandomNumber(1, 100);
     const randomSign = getRandomSign();
     const calcQuestion = `${randomNumber1} ${randomSign} ${randomNumber2}`;
-    const calcAnswer = eval(`${randomNumber1} ${randomSign} ${randomNumber2}`);
+    const calcAnswer = String(eval(`${randomNumber1} ${randomSign} ${randomNumber2}`));
     return cons(calcQuestion, calcAnswer);
 }
-brainGame();
+brainGame(questionAnswer);
