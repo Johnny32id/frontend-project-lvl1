41fdb931281f2507e74659bfd083237c8b945378
Evgeniy Slizh <johnnyhandsome3232@gmail.com>
2019-11-06 21:43:00 +0300

    modified brain-calc

--- src/bin/games/brain-calc.js
+++ src/bin/games/brain-calc.js
@@ -1,15 +1,15 @@
 #!/usr/bin/env node
-import { brainEvenRules, greetings, brainGame, getRandomNumber, getRandomSign } from '../..';
+import {greetings, brainGame, getRandomNumber, getRandomSign, brainCalcRules, randomNumbersCalculation } from '../..';
 import {cons, car, cdr, toString} from "@hexlet/pairs";
 greetings();
-brainEvenRules();
+brainCalcRules();
 
 export const getQuestionAnswer = () => {
     const randomNumber1 = getRandomNumber(1, 100);
     const randomNumber2 = getRandomNumber(1, 100);
     const randomSign = getRandomSign();
     const calcQuestion = `${randomNumber1} ${randomSign} ${randomNumber2}`;
-    const calcAnswer = String(eval(`${randomNumber1} ${randomSign} ${randomNumber2}`));
+    const calcAnswer = String(randomNumbersCalculation(randomNumber1, randomNumber2, randomSign));
     return cons(calcQuestion, calcAnswer);
 }
 brainGame(getQuestionAnswer);
