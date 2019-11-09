007a2cc943f4b23177458baa70b6504fc81bfa49
Evgeniy Slizh <johnnyhandsome3232@gmail.com>
2019-11-09 13:10:07 +0300

    modified even

---
+++ src/bin/brain-even.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+import { greetings, brainGame, brainRules } from '..';
+
+import getQuestionAnswer from '../games/brain-even-game';
+
+greetings();
+brainRules('Answer "yes" if the number is even, otherwise answer "no".');
+
+
+brainGame(getQuestionAnswer);
