6ac7ca7a4f824b146bae4b3544595c33283bb5ae
Evgeniy Slizh <johnnyhandsome3232@gmail.com>
2019-11-09 13:10:26 +0300

    add brain-even-game

---
+++ src/games/brain-even-game.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+import cons from '@hexlet/pairs';
+
+import getRandomNumber from '..';
+
+const getQuestionAnswer = () => {
+  const evenQuestion = getRandomNumber(1, 100);
+  const evenAnswer = evenQuestion % 2 === 0 ? 'yes' : 'no';
+  return cons(evenQuestion, evenAnswer);
+};
+export default getQuestionAnswer;
