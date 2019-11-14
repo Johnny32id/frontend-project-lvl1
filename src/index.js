8c8580baa60624bd4d2fd5366709b9a623f269dc
Evgeniy Slizh <johnnyhandsome3232@gmail.com>
2019-11-14 21:57:56 +0300

    modified core

--- src/index.js
+++ src/index.js
@@ -2,12 +2,11 @@ import readlineSync from 'readline-sync';
 
 import { car, cdr } from '@hexlet/pairs';
 
-export const greetings = () => (console.log('Welcome to the Brain Games!'));
-export const brainRules = (rule) => (console.log(rule));
-export const askUserName = () => (readlineSync.question('May I have your name?'));
 export const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
-export const brainGame = (getQuestionAnswer) => {
-  const name = askUserName();
+export const brainGame = (getQuestionAnswer, rule) => {
+  console.log('Welcome to the Brain Games!');
+  console.log(rule);
+  const name = readlineSync.question('May I have your name?');
   console.log(`Hello, ${name}!`);
   for (let counter = 0; counter <= 3; counter += 1) {
     if (counter === 3) {
