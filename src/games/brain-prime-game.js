67216207ee0383cb669799b1c1c820f639270f42
Evgeniy Slizh <johnnyhandsome3232@gmail.com>
2019-11-17 21:40:38 +0300

    fix divisor counts

--- src/games/brain-prime-game.js
+++ src/games/brain-prime-game.js
@@ -1,22 +1,25 @@
 import { cons } from '@hexlet/pairs';
 
-import { getRandomNumber, brainGame } from '..';
+import brainGame from '..';
+
+import getRandomNumber from '../utils';
 
 const isPrime = (value) => {
   let divisorCounter = 0;
-  for (let divisor = 1; divisor <= value; divisor += 1) {
+  for (let divisor = 1; divisor <= value / 2; divisor += 1) {
     if (value % divisor === 0) {
       divisorCounter += 1;
     }
   }
-  return divisorCounter === 2;
+  return divisorCounter === 1;
 };
 const getQuestionAnswer = () => {
   const primeQuestion = getRandomNumber(1, 1000);
   const primeAnswer = isPrime(primeQuestion) ? 'yes' : 'no';
   return cons(primeQuestion, primeAnswer);
 };
+const brainPrimeRule = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 const primeGame = () => {
-  brainGame(getQuestionAnswer, 'Answer "yes" if given number is prime. Otherwise answer "no".');
+  brainGame(getQuestionAnswer, brainPrimeRule);
 };
 export default primeGame;
