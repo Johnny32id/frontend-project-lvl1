4e07f812cc6545dc11888eb9981b3d8d6394bb5e
Evgeniy Slizh <johnnyhandsome3232@gmail.com>
2019-11-27 22:10:57 +0300

    fix brainGame

--- src/index.js
+++ src/index.js
@@ -2,17 +2,13 @@ import readlineSync from 'readline-sync';
 
 import { car, cdr } from '@hexlet/pairs';
 
+const questionsQuantity = 3;
 const brainGame = (getQuestionAnswer, rule) => {
   console.log('Welcome to the Brain Games!');
   console.log(rule);
   const name = readlineSync.question('May I have your name?');
   console.log(`Hello, ${name}!`);
-  const questionsQuantity = 3;
-  for (let counter = 0; counter <= questionsQuantity; counter += 1) {
-    if (counter === 3) {
-      console.log(`Congratulations, ${name}!`);
-      break;
-    }
+  for (let counter = 0; counter < questionsQuantity; counter += 1) {
     const questionAnswer = getQuestionAnswer();
     const question = `Question: ${car(questionAnswer)}`;
     console.log(question);
@@ -24,6 +20,8 @@ const brainGame = (getQuestionAnswer, rule) => {
       console.log(`${userAnswer} is wrong answer;(. Correct answer was ${gameAnswer}.`);
       console.log(`Let's try again, ${name}`);
       break;
+    } if (counter === questionsQuantity - 1) {
+      console.log(`Congratulations, ${name}!`);
     }
   }
 };
